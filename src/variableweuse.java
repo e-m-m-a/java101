fe2dda9f71185f248a50d181b95497c857b3ab4f
emma <emma@iknowthe.net>
2022-10-26 14:40:24 +0100

    calc

---
+++ src/variableweuse.java
@@ -0,0 +1,13 @@
+public class variableweuse {
+    public static void main (String[]args){
+        // your code here
+// create a variable
+        String petType = "Cat";
+        String petName = "Billy";
+        int weight = 2;
+
+    // print
+        System.out.println("the pet type is = " + petType);
+
+    }
+}
